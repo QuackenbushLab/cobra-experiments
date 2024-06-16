e6dff46951c8546b2f793d70b7646a1a902de59e
soel.micheletti <soel.micheletti@hotmail.it>
2024-06-17 01:19:50 +0200

    combat library

--- experiments/figure1.R
+++ experiments/figure1.R
@@ -2,6 +2,7 @@ library(MASS)
 library(ggplot2)
 library(limma)
 library(gridExtra)
+library(sva)
 
 setwd("/home/soel/cobra/figures/")
 numSamples <- 1000
@@ -11,11 +12,8 @@ Batch <- c(rep("A",numSamples),rep("B",numSamples))
 
 df11 <- data.frame(gene1,gene2,Batch, Correction="1. Raw Data", type="Example 1")
 
-correct <- c(rep(0,numSamples),rep(2,numSamples))
-scalecorrect <- c(rep(1,numSamples),rep(sqrt(2),numSamples))
-gene1_ls <- gene1-correct
-gene2_ls <- (gene2-correct-7)/scalecorrect+7
-df12 <- data.frame(gene1=gene1_ls,gene2=gene2_ls,Batch, Correction="2. Batch Corrected",type="Example 1")
+expr_combat = ComBat(dat=rbind(gene1, gene2), batch=(Batch=="A"), par.prior=TRUE, prior.plots=FALSE)
+df12 <- data.frame(gene1=expr_combat[1, ],gene2=expr_combat[2, ],Batch, Correction="2. Batch Corrected",type="Example 1")
 
 genes <- mvrnorm(n = numSamples, c(7,7), matrix(c(1,.95,.95,1),nrow=2), tol = 1e-6, empirical = FALSE, EISPACK = FALSE)
 gene1 <- c(genes[,1],rnorm(numSamples,9))
@@ -23,7 +21,8 @@ gene2 <- c(genes[,2],rnorm(numSamples,9))
 
 
 df21 <- data.frame(gene1=gene1,gene2=gene2,Batch, Correction="1. Raw Data", type="Example 2")
-df22 <- data.frame(gene1=gene1-correct,gene2=gene2-correct,Batch, Correction="2. Batch Corrected", type="Example 2")
+expr_combat = ComBat(dat=rbind(gene1, gene2), batch=(Batch=="A"), par.prior=TRUE, prior.plots=FALSE)
+df22 <- data.frame(gene1=expr_combat[1, ],gene2=expr_combat[2, ],Batch, Correction="2. Batch Corrected", type="Example 2")
 
 df<- rbind(df11,df12,df21,df22)
 ggplot(df, aes(x=gene1,y=gene2)) +geom_point(aes(col=Batch),size=3, alpha=.5) +
